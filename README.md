0f64013e0e9cde5ed0443a2e9bd6d6409f04402c
root <root@fv-az238-680.lrtv1j1tcsgurjp4mtmc3vvjre.gx.internal.cloudapp.net>
2021-04-19 15:37:41 +0000

    Add extras for sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1609930834433 release-keys
    Signed-off-by: root <root@fv-az238-680.lrtv1j1tcsgurjp4mtmc3vvjre.gx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1609930834433 release-keys
+- Manufacturer: alps
+- Platform: mt6768
+- Codename: oppo6769
+- Brand: alps
+- Flavor: sys_oplus_mssi_64_cn-user
+- Release Version: 11
+- Id: RP1A.200720.011
+- Incremental: 1609930834433
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: alps/vnd_oppo6769/oppo6769:11/RP1A.200720.011/1609930834433:user/release-keys
+- OTA version: 
+- Branch: sys_oplus_mssi_64_cn-user-11-RP1A.200720.011-1609930834433-release-keys
+- Repo: alps_oppo6769_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ odm/bin/engineer_vendor_shell.sh
@@ -0,0 +1,115 @@
+#!/vendor/bin/sh
+
+config="$1"
+
+function doAddRadioFile(){
+    if [[ -d /mnt/vendor/opporeserve/radio ]]; then
+        if [[ ! -f /mnt/vendor/opporeserve/radio/exp_operator_switch.config ]]; then
+            touch /mnt/vendor/opporeserve/radio/exp_operator_switch.config
+        fi
+        if [[ ! -f /mnt/vendor/opporeserve/radio/exp_region_netlock.config ]]; then
+            touch /mnt/vendor/opporeserve/radio/exp_region_netlock.config
+        fi
+        if [[ ! -f /mnt/vendor/opporeserve/radio/exp_operator_simlock_switch.config ]]; then
+            touch /mnt/vendor/opporeserve/radio/exp_operator_simlock_switch.config
+        fi
+        if [[ ! -f /mnt/vendor/opporeserve/radio/exp_operator_simlock_times.config ]]; then
+            touch /mnt/vendor/opporeserve/radio/exp_operator_simlock_times.config
+        fi
+        if [[ ! -f /mnt/vendor/opporeserve/radio/exp_sim_operator_switch.config ]]; then
+            touch /mnt/vendor/opporeserve/radio/exp_sim_operator_switch.config
+        fi
+        if [[ ! -f /mnt/vendor/opporeserve/radio/exp_open_market_singlecard.config ]]; then
+            touch /mnt/vendor/opporeserve/radio/exp_open_market_singlecard.config
+        fi
+
+        chown radio system /mnt/vendor/opporeserve/radio/exp_operator_switch.config
+        chown radio system /mnt/vendor/opporeserve/radio/exp_region_netlock.config
+        chown radio system /mnt/vendor/opporeserve/radio/exp_operator_simlock_switch.config
+        chown radio system /mnt/vendor/opporeserve/radio/exp_operator_simlock_times.config
+        chown radio system /mnt/vendor/opporeserve/radio/exp_sim_operator_switch.config
+        chown radio system /mnt/vendor/opporeserve/radio/exp_open_market_singlecard.config
+
+
+        chmod 0660 /mnt/vendor/opporeserve/radio/exp_operator_switch.config
+        chmod 0660 /mnt/vendor/opporeserve/radio/exp_region_netlock.config
+        chmod 0660 /mnt/vendor/opporeserve/radio/exp_operator_simlock_switch.config
+        chmod 0660 /mnt/vendor/opporeserve/radio/exp_operator_simlock_times.config
+        chmod 0660 /mnt/vendor/opporeserve/radio/exp_sim_operator_switch.config
+        chmod 0660 /mnt/vendor/opporeserve/radio/exp_open_market_singlecard.config
+
+    fi
+    if [[ -d /mnt/vendor/oplusreserve/radio ]]; then
+        if [[ ! -f /mnt/vendor/oplusreserve/radio/exp_operator_switch.config ]]; then
+            touch /mnt/vendor/oplusreserve/radio/exp_operator_switch.config
+        fi
+        if [[ ! -f /mnt/vendor/oplusreserve/radio/exp_region_netlock.config ]]; then
+            touch /mnt/vendor/oplusreserve/radio/exp_region_netlock.config
+        fi
+        if [[ ! -f /mnt/vendor/oplusreserve/radio/exp_operator_simlock_switch.config ]]; then
+            touch /mnt/vendor/oplusreserve/radio/exp_operator_simlock_switch.config
+        fi
+        if [[ ! -f /mnt/vendor/oplusreserve/radio/exp_operator_simlock_times.config ]]; then
+            touch /mnt/vendor/oplusreserve/radio/exp_operator_simlock_times.config
+        fi
+        if [[ ! -f /mnt/vendor/oplusreserve/radio/exp_sim_operator_switch.config ]]; then
+            touch /mnt/vendor/oplusreserve/radio/exp_sim_operator_switch.config
+        fi
+        if [[ ! -f /mnt/vendor/oplusreserve/radio/exp_open_market_singlecard.config ]]; then
+            touch /mnt/vendor/oplusreserve/radio/exp_open_market_singlecard.config
+        fi
+
+        chown radio system /mnt/vendor/oplusreserve/radio/exp_operator_switch.config
+        chown radio system /mnt/vendor/oplusreserve/radio/exp_region_netlock.config
+        chown radio system /mnt/vendor/oplusreserve/radio/exp_operator_simlock_switch.config
+        chown radio system /mnt/vendor/oplusreserve/radio/exp_operator_simlock_times.config
+        chown radio system /mnt/vendor/oplusreserve/radio/exp_sim_operator_switch.config
+        chown radio system /mnt/vendor/oplusreserve/radio/exp_open_market_singlecard.config
+
+
+        chmod 0660 /mnt/vendor/oplusreserve/radio/exp_operator_switch.config
+        chmod 0660 /mnt/vendor/oplusreserve/radio/exp_region_netlock.config
+        chmod 0660 /mnt/vendor/oplusreserve/radio/exp_operator_simlock_switch.config
+        chmod 0660 /mnt/vendor/oplusreserve/radio/exp_operator_simlock_times.config
+        chmod 0660 /mnt/vendor/oplusreserve/radio/exp_sim_operator_switch.config
+        chmod 0660 /mnt/vendor/oplusreserve/radio/exp_open_market_singlecard.config
+
+    fi
+}
+
+function doStartDiagSocketLog {
+    ip_address=`getprop vendor.oppo.diag.socket.ip`
+    port=`getprop vendor.oppo.diag.socket.port`
+    retry=`getprop vendor.oppo.diag.socket.retry`
+    channel=`getprop vendor.oppo.diag.socket.channel`
+    if [[ -z "${ip_address}" ]]; then
+        ip_address=181.157.1.200
+    fi
+    if [[ -z "${port}" ]]; then
+        port=2500
+    fi
+    if [[ -z "${retry}" ]]; then
+        port=10000
+    fi
+    if [[ -z "${channel}" ]]; then
+        diag_socket_log -a ${ip_address} -p ${port} -r ${retry}
+    else
+        diag_socket_log -a ${ip_address} -p ${port} -r ${retry} -c ${channel}
+    fi
+}
+
+function doStopDiagSocketLog {
+    diag_socket_log -k
+}
+
+case "$config" in
+    "addRadioFile")
+    doAddRadioFile
+    ;;
+    "startDiagSocketLog")
+    doStartDiagSocketLog
+    ;;
+    "stopDiagSocketLog")
+    doStopDiagSocketLog
+    ;;
+esac
